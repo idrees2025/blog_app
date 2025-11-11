77ddd8c5c3c7b4fb6cac26e4b959717d0a018a9b
idrees2025 <shahjahanidrees449@gmail.com>
2025-11-11 08:11:43 +0000

    initilation

--- blog_site/asgi.py
+++ blog_site/asgi.py
@@ -13,4 +13,4 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_site.settings')
 
-application = get_asgi_application()
+application = get_asgi_application()git branch -M maingit push -u origin main
